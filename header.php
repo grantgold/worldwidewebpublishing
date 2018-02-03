fb70cecf34104c554d344e8243879cde2dd1fd9b
grantgold <grantagold@gmail.com>
2018-02-03 16:12:09 -0500

    Adds files

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>WorldWideWebPublishingCompany</title>
+    <meta name="description" content="Digital Art Book Publishing Company" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Fonts: Lato / http://www.latofonts.com/ -->
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+
+    <!-- Font Awesome // http://fortawesome.github.io/Font-Awesome/icons -->
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css"/>
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" media="screen, print" href="style.css"/>
+
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+</head>
+<body class="fadein">
